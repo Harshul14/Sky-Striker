665b7c9945f4e9c0dd04ef1b4a0ba787c29e4d75
Harshul14 <Harshul147@gmail.com>
2023-12-22 12:54:40 +0530

    splash screen and logo

--- app/src/main/java/com/developer/harshul/skystriker/MainActivity.java
+++ app/src/main/java/com/developer/harshul/skystriker/MainActivity.java
@@ -3,20 +3,37 @@ package com.developer.harshul.skystriker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        imageView.setVisibility(View.INVISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.VISIBLE);
+            }
+        }, 1500);
     }
-
     public void startGame(View V) {
         Log.i("ImageButton", "clicked");
-        Intent intent=new Intent(this,StartGame.class);
+        Intent intent = new Intent(this, StartGame.class);
         startActivity(intent);
         finish();
     }
